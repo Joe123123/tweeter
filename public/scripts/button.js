bbfc269b1975e90f16018ee4fe2bc158068e5807
Zhao Wang <wangz2266@gmail.com>
2020-02-26 09:26:33 -0800

    event handle for button

---
+++ public/scripts/button.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  let headerButton = $("#btn-header");
+  const upDownLoop = () => {
+    headerButton.animate(
+      { top: "55px" },
+      {
+        duration: 800,
+        complete: function() {
+          headerButton.animate(
+            { top: "50px" },
+            { duration: 800, complete: upDownLoop }
+          );
+        }
+      }
+    );
+  };
+  upDownLoop();
+  headerButton.click(function() {
+    $(".new-tweet").slideToggle("slow");
+    $(".new-tweet textarea").focus();
+  });
+
+  let $scrollUpButton = $("#btn-scroll-top");
+
+  $(document).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+      $scrollUpButton.addClass("show");
+    } else {
+      $scrollUpButton.removeClass("show");
+    }
+  });
+
+  $scrollUpButton.on("click", function(e) {
+    e.preventDefault();
+    $(document).scrollTop(0);
+    $(".new-tweet").show("slow");
+    $(".new-tweet textarea").focus();
+  });
+});
